309ea28240c3034956f8d61b537fcfa65eced586
Tanner <Tanner@DESKTOP-SBM2OA1>
2021-01-31 11:26:08 -0600

    Added LSTM Model for price preidction

--- JSON_To_CSV.py
+++ JSON_To_CSV.py
@@ -1,10 +1,12 @@
 import json 
-import csv 
+import csv
+import os 
 from urllib.request import urlopen
 import pandas as pd
 import requests
+import time
 
-solditems = requests.get('https://secure.runescape.com/m=itemdb_rs/api/graph/11720.json') # (your url)
+solditems = requests.get('https://secure.runescape.com/m=itemdb_rs/api/graph/11718.json') # (your url)
 data = solditems.json()
 with open('data.json', 'w') as f:
     json.dump(data, f)
@@ -12,7 +14,8 @@ with open('data.json', 'w') as f:
 df = pd.read_json (r'data.json')
 export_csv = df.to_csv (r'data.csv', index = None, header=True)
 
-reader = csv.reader(open('data.csv', 'r'))
+fr = open('data.csv', 'r')
+reader = csv.reader(fr)
 writer = csv.writer(open('data_final.csv', 'w'))
 headers = next(reader)
 headers.append("day")
@@ -22,3 +25,20 @@ for row in reader:
     row.append(str(count))
     writer.writerow(row)
     count += 1
+
+fr.close()
+os.remove('data.csv')
+
+def csvAdder():
+    fr = open('message.csv', 'r')
+    reader = csv.reader(fr)
+    writer = csv.writer(open('messages_final.csv', 'w'))
+    headers = next(reader)
+    headers.append("day")
+    writer.writerow(headers)
+    count = 1
+    for row in reader:
+        row.append(str(count))
+        writer.writerow(row)
+        count += 1
+
---
+++ LSTM.py
@@ -0,0 +1,119 @@
+# LSTM for international airline passengers problem with time step regression framing
+from operator import concat
+import numpy
+import matplotlib.pyplot as plt
+from pandas import read_csv
+import math
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+import pandas
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+# convert an array of values into a dataset matrix
+def create_dataset(dataset, look_back=3):
+	dataX, dataY = [], []
+	for i in range(len(dataset)-look_back-1):
+		a = dataset[i:(i+look_back), 0]
+		dataX.append(a)
+		dataY.append(dataset[i + look_back, 0])
+	return numpy.array(dataX), numpy.array(dataY)
+# fix random seed for reproducibility
+numpy.random.seed(7)
+# load the dataset
+dataframe = read_csv('data_final.csv', usecols=[0], engine='python')
+dataset = dataframe.values
+dataset = dataset.astype('float32')
+# normalize the dataset
+scaler = MinMaxScaler(feature_range=(0, 1))
+dataset = scaler.fit_transform(dataset)
+# split into train and test sets
+train_size = int(len(dataset) * 0.7)
+test_size = len(dataset) - train_size
+train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+# reshape into X=t and Y=t+1
+look_back = 3
+trainX, trainY = create_dataset(train, look_back)
+testX, testY = create_dataset(test, look_back)
+# reshape input to be [samples, time steps, features]
+trainX = numpy.reshape(trainX, (trainX.shape[0], trainX.shape[1], 1))
+testX = numpy.reshape(testX, (testX.shape[0], testX.shape[1], 1))
+# create and fit the LSTM network
+model = Sequential()
+model.add(LSTM(4, input_shape=(look_back, 1)))
+model.add(Dense(1))
+model.compile(loss='mean_squared_error', optimizer='adam')
+model.fit(trainX, trainY, epochs=150, batch_size=1, verbose=2)
+# make predictions
+trainPredict = model.predict(trainX)
+testPredict = model.predict(testX)
+# invert predictions
+trainPredict = scaler.inverse_transform(trainPredict)
+trainY = scaler.inverse_transform([trainY])
+testPredict = scaler.inverse_transform(testPredict)
+testY = scaler.inverse_transform([testY])
+# calculate root mean squared error
+trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
+print('Train Score: %.2f RMSE' % (trainScore))
+testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
+print('Test Score: %.2f RMSE' % (testScore))
+# shift train predictions for plotting
+trainPredictPlot = numpy.empty_like(dataset)
+trainPredictPlot[:, :] = numpy.nan
+trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+# shift test predictions for plotting
+testPredictPlot = numpy.empty_like(dataset)
+testPredictPlot[:, :] = numpy.nan
+testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
+# plot baseline and predictions
+plt.plot(scaler.inverse_transform(dataset))
+plt.plot(trainPredictPlot)
+plt.plot(testPredictPlot)
+plt.show()
+
+from sklearn.preprocessing import StandardScaler
+data = pandas.read_csv('data_final.csv')
+data.head()
+
+df_new = data[['daily']]
+last_30_days = df_new[-30:].values
+
+scaler = MinMaxScaler(feature_range=(0, 1))
+
+last_30_days_scaled = scaler.fit_transform(last_30_days)
+
+X_test = []
+X_test.append(last_30_days_scaled)
+X_test_np = numpy.array(X_test)
+
+X_test_np = numpy.reshape(X_test_np, (X_test_np.shape[0], X_test_np.shape[1],1))
+
+pred_price = model.predict(X_test_np)
+pred_price = scaler.inverse_transform(pred_price)
+
+print('Est Price: ' + str(pred_price))
+new_Pred = pred_price
+
+for x in range(5):
+	str_pred = str(new_Pred)
+	str_pred = str_pred.replace('[[', '')
+	str_pred = str_pred.replace(']]', '')
+	df_new.loc[len(df_new)] = float(str_pred)
+	df_new.index = df_new.index + x  # shifting index
+	df_new = df_new.sort_index()
+	last_30_days = df_new[-30:].values
+
+	scaler = MinMaxScaler(feature_range=(0, 1))
+
+	last_30_days_scaled = scaler.fit_transform(last_30_days)
+
+	X_test = []
+	X_test.append(last_30_days_scaled)
+	X_test_np = numpy.array(X_test)
+
+	X_test_np = numpy.reshape(X_test_np, (X_test_np.shape[0], X_test_np.shape[1],1))
+
+	pred_price = model.predict(X_test_np)
+	pred_price = scaler.inverse_transform(pred_price)
+	new_Pred = pred_price
+	print('Est Price: ' + str(pred_price))
