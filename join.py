e1e0e3b0594bd692c0fc1f31ddcfd2e216a7c9d8
Greg Morgan <gamm90@gmail.com>
2021-02-02 14:04:30 -0600

    Create join.py

---
+++ join.py
@@ -0,0 +1,53 @@
+import csv
+import glob
+import os
+
+os.remove('data_joined_predictions.csv')
+
+predictions = glob.glob('*predictions.csv')
+headers = []
+headers.append('day')
+
+#open all of the files.
+readers = []
+for p in predictions:
+    f = open(p, 'r')
+    readers.append(f)
+#read the cell from all of the files at once.
+content = {}
+for reader in readers:
+    content[reader.name] = []
+    for row in reader:
+        content[reader.name].append(row)
+
+outputfile = open('data_joined_predictions.csv', 'w')
+writer = csv.writer(outputfile)
+
+#write headers to new file.
+for prediction in predictions:
+    itemID = str(prediction
+        .replace('data_', '')
+        .replace('_predictions.csv', ''))
+    headers.append(itemID + ' daily')
+    headers.append(itemID + ' avg')
+writer.writerow(headers)
+
+#build a row from all of the cells
+row = []
+for i in range(1,191):
+    row.append(i-1)
+    for k in content:
+        lines = content[k]
+        daily = lines[i].split(',')[1]
+        average = lines[i].split(',')[2]
+        row.append(daily)
+        row.append(average)
+    
+    
+    #write row to new file.
+    writer.writerow(row)
+    #clear row
+    row = []
+
+outputfile.flush()
+outputfile.close()
